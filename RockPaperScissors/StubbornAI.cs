f92c82e7a18d947534b2d78c5ac2610f352bce48
Echarger1 <echarger1@gmail.com>
2016-07-01 12:10:46 -0400

    Added ReadMe

---
+++ RockPaperScissors/StubbornAI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class StubbornAI : IPlayer
+    {
+        public StubbornAI(int Favoritemove)
+        {
+            favoritemove = Favoritemove;
+        }
+        int favoritemove = 0;
+        public int NextMove()
+        {
+            return favoritemove;
+        }
+
+        public void SaveResult(int myMove, int otherMove)
+        {
+        }
+    }
+}
